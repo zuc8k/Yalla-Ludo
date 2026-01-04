590e948501af19d0908334dc695c4006bd5c860c
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 10:22:45 +0200

    Update index.js

--- Game Server/src/sockets/index.js
+++ Game Server/src/sockets/index.js
@@ -1,10 +1,19 @@
+const matchmaking = require("../matchmaking/matchmaking");
+
 module.exports = (io) => {
   io.on("connection", socket => {
     console.log("🎮 Player Connected:", socket.id);
 
-    require("./game.socket")(io, socket);
+    socket.on("find_match", () => {
+      matchmaking.joinQueue(io, socket);
+    });
+
+    socket.on("cancel_match", () => {
+      matchmaking.leaveQueue(socket);
+    });
 
     socket.on("disconnect", () => {
+      matchmaking.leaveQueue(socket);
       console.log("❌ Player Disconnected:", socket.id);
     });
   });
