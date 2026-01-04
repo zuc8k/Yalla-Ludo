0bae930640d2a0a5e2850660264e8d9086b0bcd1
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 18:04:09 +0200

    Delete Game Server / Discord Bot – config directory

--- Game Server / Discord Bot – config/ranks.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  { name: "Bronze", min: 0 },
-  { name: "Silver", min: 500 },
-  { name: "Gold", min: 1200 },
-  { name: "Platinum", min: 2000 },
-  { name: "Diamond", min: 3000 },
-  { name: "Master", min: 4500 }
-];
