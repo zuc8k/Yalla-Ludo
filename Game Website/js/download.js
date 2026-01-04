bd8f30e9aa1d98b76291dd6d358e78acb417aa50
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 10:01:42 +0200

    Create download.js

---
+++ Game Website/js/download.js
@@ -0,0 +1,5 @@
+document.getElementById("androidBtn").href =
+  "https://your-site.com/game.apk";   // APK أو Play Store
+
+document.getElementById("iosBtn").href =
+  "https://apps.apple.com/app/idXXXX"; // App Store
