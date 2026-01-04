1fd7a66ebe1140aea2a70ec14877dfb3411b34b7
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 14:06:14 +0200

    Create ranks.config.js

---
+++ Game Server/src/rank/ranks.config.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { name: "Bronze", min: 0, badge: "🥉" },
+  { name: "Silver", min: 500, badge: "🥈" },
+  { name: "Gold", min: 1200, badge: "🥇" },
+  { name: "Platinum", min: 2000, badge: "💎" },
+  { name: "Diamond", min: 3000, badge: "🔷" },
+  { name: "Master", min: 4500, badge: "👑" }
+];
