469138827a2e4b790ed0ee246cdee5aae20ce239
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 11:42:33 +0200

    Update auth.routes.js

--- Game Server/src/routes/auth.routes.js
+++ Game Server/src/routes/auth.routes.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 const hwidCheck = require("../middlewares/hwid.middleware");
 
+// Register + Login مع فحص HWID
 router.post("/register", hwidCheck, auth.register);
 router.post("/login", hwidCheck, auth.login);
-
-router.post("/register", auth.register);
-router.post("/login", auth.login);
 
 module.exports = router;
