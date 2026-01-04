e79c1ef111561ea2a01407f85a48111a12e88585
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 09:57:15 +0200

    Create ApiConfig.cs

---
+++ Assets/Scripts/Config/ApiConfig.cs
@@ -0,0 +1,8 @@
+namespace Game.Config
+{
+    public static class ApiConfig
+    {
+        public static string BASE_URL = "http://localhost:3000/api";
+        // لو سيرفر أونلاين غير اللينك
+    }
+}
