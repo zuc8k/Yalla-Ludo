783db850fc22336ece93363c9b7cd6ce0f7a943d
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-04 14:01:13 +0200

    Create ranks.js

---
+++ Game Server / Discord Bot – config/ranks.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { name: "Bronze", min: 0 },
+  { name: "Silver", min: 500 },
+  { name: "Gold", min: 1200 },
+  { name: "Platinum", min: 2000 },
+  { name: "Diamond", min: 3000 },
+  { name: "Master", min: 4500 }
+];
